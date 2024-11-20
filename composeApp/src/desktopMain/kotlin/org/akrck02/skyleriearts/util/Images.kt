cc69dbf5c95be5d6cd939132fe3861a130b391cc
akrck02 <aketza.vazquez@gmail.com>
2024-11-20 20:56:08 +0100

    improvement: new dynamic image grid and surface use

---
+++ composeApp/src/desktopMain/kotlin/org/akrck02/skyleriearts/util/Images.kt
@@ -0,0 +1,13 @@
+package org.akrck02.skyleriearts.util
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import org.jetbrains.skia.Image
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+fun loadImageFrom(filePath: String): ImageBitmap {
+    val bytes = Files.readAllBytes(Path.of(filePath)) // path relative to project root
+    return Image.makeFromEncoded(bytes).toComposeImageBitmap()
+}
